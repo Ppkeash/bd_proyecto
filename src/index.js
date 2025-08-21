3ea10b6357f7da22ceb48924f662d8cb2885f091
Ppkeash <71032118+Ppkeash@users.noreply.github.com>
2025-08-21 13:40:21 -0500

    Add Requests history page with filters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./styles.css";
 
 // Punto de entrada de la aplicación React
 
 import App from "./App";
+import RequestsHistory from "./pages/RequestsHistory";
 import { ToastProvider } from "./components/ui/ToastProvider";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <ToastProvider>
-      <App />
+      <BrowserRouter>
+        <div className="p-4 flex gap-4 border-b">
+          <Link to="/">Inicio</Link>
+          <Link to="/requests">Historial</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/requests" element={<RequestsHistory />} />
+        </Routes>
+      </BrowserRouter>
     </ToastProvider>
   </StrictMode>
 );
